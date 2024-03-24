b725ddafb4a77bca77db5100f0ca1e37a2360114
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-03-24 16:30:10 +0100

    feat: Solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,13 +1,120 @@
 package org.tfoc;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * The class containing the solution to this exercise
  */
+@Slf4j
 public class Solution {
 
     public int orangesRotting(int[][] grid) {
 
-        return 0;
+        List<Cell> cells = new ArrayList<>();
+        int numTotalFreshOranges = 0;
+        int numTotalRottenOranges = 0;
+
+        int total = 0;
+
+        // Buscamos todas las naranjas podridas
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[row].length; column++) {
+                if (grid[row][column] == 2)
+                    cells.add(new Cell(column, row));
+
+                if (grid[row][column] == 1)
+                    numTotalFreshOranges++;
+
+            }
+        }
+
+        while (!cells.isEmpty()) {
+            cells = calculateNextMinute(grid, cells);
+            numTotalRottenOranges = numTotalRottenOranges + cells.size();
+            if (!cells.isEmpty())
+                total++;
+        }
+
+
+        return numTotalRottenOranges == numTotalFreshOranges
+                ? total : -1;
+    }
+
+    public boolean isCellWithFreshOrange(int[][] grid, Cell cell) {
+        return Objects.nonNull(cell) && grid[cell.row][cell.column] == 1;
+    }
+
+    public void setCellWithRottenOrange(int[][] grid, Cell cell) {
+        grid[cell.row][cell.column] = 2;
+    }
+
+    public List<Cell> calculateNextMinute(int[][] grid, List<Cell> cellsRottenOranges) {
+
+        List<Cell> numRottenOranges = new ArrayList<>();
+
+        for (Cell cell : cellsRottenOranges) {
+
+            Cell rightCell = getRightCell(grid[cell.row].length, cell);
+            if (isCellWithFreshOrange(grid, rightCell)) {
+                numRottenOranges.add(rightCell);
+                setCellWithRottenOrange(grid, rightCell);
+            }
+
+            Cell leftCell = getLeftCell(cell);
+            if (isCellWithFreshOrange(grid, leftCell)) {
+                numRottenOranges.add(leftCell);
+                setCellWithRottenOrange(grid, leftCell);
+            }
+
+            Cell upCell = getUpCell(cell);
+            if (isCellWithFreshOrange(grid, upCell)) {
+                numRottenOranges.add(upCell);
+                setCellWithRottenOrange(grid, upCell);
+            }
+
+            Cell downCell = getDownCell(grid.length, cell);
+            if (isCellWithFreshOrange(grid, downCell)) {
+                numRottenOranges.add(downCell);
+                setCellWithRottenOrange(grid, downCell);
+            }
+
+        }
+
+        return numRottenOranges;
+
+    }
+
+
+    public Cell getRightCell(int n, Cell actualCell) {
+        if (actualCell.column + 1 < n)
+            return new Cell(actualCell.column + 1, actualCell.row);
+
+        return null;
+    }
+
+    public Cell getLeftCell(Cell actualCell) {
+        if (actualCell.column - 1 >= 0)
+            return new Cell(actualCell.column - 1, actualCell.row);
+        return null;
+    }
+
+    public Cell getUpCell(Cell actualCell) {
+        if (actualCell.row - 1 >= 0)
+            return new Cell(actualCell.column, actualCell.row - 1);
+        return null;
+    }
+
+    public Cell getDownCell(int m, Cell actualCell) {
+        if (actualCell.row + 1 < m)
+            return new Cell(actualCell.column, actualCell.row + 1);
+        return null;
+    }
+
+    public record Cell(int column, int row) {
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,32 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    private static Stream<Arguments> provideGrid() {
+        return Stream.of(
+                Arguments.of(new int[][]{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}}, 4),
+                Arguments.of(new int[][]{{0, 2}}, 0),
+                Arguments.of(new int[][]{{1, 1, 1}, {1, 2, 1}, {1, 1, 1}}, 2)
+        );
+    }
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @ParameterizedTest
+    @MethodSource("provideGrid")
+    void testSolution(int[][] grid, int expected) {
+        Solution solution = new Solution();
+
+        int i = solution.orangesRotting(grid);
+
+        Assertions.assertEquals(expected, i);
     }
 
 }
